951db9f2e31d50ceae71765abdc794512e14d114
fahrurozi <m.fahruu.rozii@gmail.com>
2023-03-18 19:23:11 +0700

    add layouts template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vertical Navbar - Mazer Admin Dashboard</title>
+
+    <link rel="stylesheet" href="{{ asset('/assets/css/main/app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/main/app-dark.css') }}" />
+
+    <link rel="shortcut icon" href="{{ asset('/assets/images/logo/favicon.svg') }}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ asset('/assets/images/logo/favicon.png') }}" type="image/png" />
+    @livewireStyles
+</head>
+
+<body>
+    <div id="app">
+        @include('layouts.components.sidebar')
+        <div id="main" class="layout-navbar navbar-fixed">
+            @include('layouts.components.navbar')
+            <div id="main-content">
+                <div class="page-heading">
+                    @include('layouts.components.page_title')
+                    <section class="section">
+                       
+                    </section>
+                </div>
+
+                @include('layouts.components.footer')
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('assets/js/bootstrap.js')}}"></script>
+    <script src="{{ asset('assets/js/app.js')}}"></script>
+    @livewireScripts
+</body>
+
+</html>
